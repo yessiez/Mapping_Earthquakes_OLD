66b7a85fb9ea911dbe1c494f7b5668b6e90388a7
Jess <jess.margaret.sanchez@gmail.com>
2021-07-20 21:51:03 -0600

    Add files via upload

---
+++ Mapping_Multiple_Points/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoieWVzc2lleiIsImEiOiJja3IxMG1obmswMDJrMm5uMzd3ZjExZDY2In0.-MHb_V6zDsCwzYMenWBohQ";
---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,45 @@
+// Add console.log to check to see if our code is working 
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    //L.marker(city.location).addTo(map);
+});
+
+
+// Add the location of each city to the map when you iterate through the cities array. 
+cityData.forEach(function(city) {
+    console.log(city)
+    //L.marker(city.location)
+    L.circleMarker(city.location, {
+        color: 'orange',
+        fillColor: '#FFA500.',
+        weight: 4,
+        fillOpacity: 0.5,
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "<h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});   
+
+
+// We create the tile layer that will be the background of our map.
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+
