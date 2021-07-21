32849a044c74a50179a5760fd63d6e5a608106a0
yessiez <jess.margaret.sanchez@gmail.com>
2021-07-20 19:31:09 -0600

    Adding files for mapping single points.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -12,9 +12,31 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //    zoom: 4
 //  });
 
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map for Los Angeles, California
+// L.circle([34.0522, -118.2437], {
+//    radius: 100
+// }).addTo(map);
+
+//SKILL DRILL
+// Using the Leaflet documentation, create a light-yellow circle 
+// with black lines indicating a 300-meter radius of Central Los Angeles on a dark map.
+/*L.circle([34.0522, -118.2437], {
+    radius: 300, 
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);*/
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300, 
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
